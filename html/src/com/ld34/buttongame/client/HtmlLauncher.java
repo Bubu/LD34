6223ccfbc3b865a7e82c76bd0dc457cbf47bded0
Marcus Hoffmann <bubuiic@aol.com>
2015-12-12 15:00:32 +0100

    Initial commit.

---
+++ html/src/com/ld34/buttongame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ld34.buttongame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ld34.buttongame.ButtonGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ButtonGame();
+        }
+}
