769ef22ead00a8982c75321409c6a00d3efdb188
Stephan Wäldchen <st.wald@protonmail.ch>
2015-12-12 19:46:45 +0100

    physics

--- core/src/com/ld34/buttongame/MassiveObject.java
+++ core/src/com/ld34/buttongame/MassiveObject.java
@@ -1,25 +1,31 @@
 package com.ld34.buttongame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class MassiveObject {
+public class MassiveObject{
 
 	public Sprite sprite;
 	private BodyDef bodyDef;
 	private com.badlogic.gdx.physics.box2d.PolygonShape shape;
 	private FixtureDef fixtureDef;
-	private Body body;
+	public Body body;
+	public float width;
+	public float height;
 
 	public MassiveObject(String graphicName)
 	{
-		Texture img = new Texture(graphicName);
+		Texture img = new Texture(Gdx.files.internal(graphicName));
 		this.sprite = new Sprite(img);
+		this.height = this.sprite.getHeight();
+		this.width = this.sprite.getWidth();
 		
 		this.bodyDef = new BodyDef();
 		this.bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -30,7 +36,6 @@ public class MassiveObject {
 		this.fixtureDef = new FixtureDef();
 		this.fixtureDef.shape = shape;
 		this.fixtureDef.density = 1f;
-		shape.dispose();
 	}
 	
 	void createHere(World world, float xPos, float yPos)
@@ -40,6 +45,15 @@ public class MassiveObject {
 		
 		this.body = world.createBody(bodyDef);
 		this.body.createFixture(this.fixtureDef);
+	}
+	
+	void draw(SpriteBatch batch)
+	{
+		sprite.setPosition(body.getPosition().x, body.getPosition().y);
+		sprite.setRotation((float)Math.toDegrees(body.getAngle()));
+		batch.draw(sprite, sprite.getX(), sprite.getY(),sprite.getOriginX(),
+                sprite.getOriginY(), sprite.getWidth(),sprite.getHeight(),
+                sprite.getScaleX(),sprite.getScaleY(),sprite.getRotation());
 	}
 	
 }
